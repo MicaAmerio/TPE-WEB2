f4e61477938de1a6352eebaf039dacd9a2f00f3e
unknown <anakarenrodriguezconte@gmail.com>
2024-10-02 16:51:39 -0300

    se creó el archivo routher.php

---
+++ router.php
@@ -0,0 +1,18 @@
+<?php
+
+
+define('BASE_URL', '//'.$_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']).'/'); 
+if (empty($_GET['action'])) { 
+    $_GET['action'] = 'home';
+} 
+
+$accion = $_GET['action']; $parametros = explode('/', $accion); 
+
+switch ($parametros[0]) { 
+    case 'home' :
+        $controller = new ProductController();
+    default: 
+    $controller = new ProductController();
+    $controller->show404();
+    break; 
+}
